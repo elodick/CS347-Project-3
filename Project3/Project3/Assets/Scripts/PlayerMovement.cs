93c2473aa955edb81994e7cd5771fd0ec94d25e5
jfletch1088 <46976616+jfletch1088@users.noreply.github.com>
2021-04-13 09:04:34 -0500

    changed to work with Unity 2019.2.8f1

--- Project3/Project3/Assets/Scripts/PlayerMovement.cs
+++ Project3/Project3/Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,7 +9,7 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
