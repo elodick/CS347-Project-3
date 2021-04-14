48dcd2ced900ecbe75a1b69dae3974094e0d0356
thedillster <81203444+thedillster@users.noreply.github.com>
2021-04-13 22:06:26 -0500

    basic level
    small room and begun building enemy

---
+++ Project3/Project3/Assets/Scripts/BasicEnemy.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField]
+    public GameObject bulletPrefab;
+
+    [SerializeField]
+    public float bulletForce = 20f;
+
+    public int Health;
+    public float AttackDelay = 3f;
+
+    public float AttackTime;
+
+    private bool shootDir;
+    void Start()
+    {
+        AttackTime = AttackDelay;    
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(AttackTime <=0)
+        {
+            if (shootDir == true)
+            {
+                shootDir = false;
+            }
+            else
+            {
+                shootDir = true;
+            }
+            Shoot(shootDir);
+            AttackTime = AttackDelay;
+        }
+    }
+    private void Shoot(bool shootDir)
+    {
+        if(shootDir == true)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+    
+}
