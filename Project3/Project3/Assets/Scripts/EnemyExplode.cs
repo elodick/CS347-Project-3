46f6de845e05d3db0e372edba8fa5851d3f06015
elodick <74278780+elodick@users.noreply.github.com>
2021-04-15 23:02:33 -0500

    sigh

---
+++ Project3/Project3/Assets/Scripts/EnemyExplode.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMelee : MonoBehaviour
+{
+    public GameObject explosion;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject hit = Instantiate(explosion, transform.position, Quaternion.identity);
+        Destroy(hit, 5f);
+        Destroy(gameObject);
+    }
+}
