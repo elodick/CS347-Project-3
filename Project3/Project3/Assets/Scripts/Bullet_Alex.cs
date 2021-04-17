d8f18d32d8c64d6855605af3af7406e5f49437e4
jfletch1088 <46976616+jfletch1088@users.noreply.github.com>
2021-04-16 19:21:54 -0500

    name changes

---
+++ Project3/Project3/Assets/Scripts/Bullet_Alex.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject hitEffect;
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Physics2D.IgnoreCollision(collision.collider, this.GetComponent<BoxCollider2D>());
+        }
+        else if (collision.gameObject.CompareTag("Enemy") || collision.gameObject.CompareTag("Wall"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
