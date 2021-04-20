bc5ebf7158edb2bf6e5d10a98c395397978f72d9
elodick <74278780+elodick@users.noreply.github.com>
2021-04-20 13:52:24 -0500

    fixed menus

---
+++ Project3/Project3/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public int minSceneIndex = 1;
+    public int maxSceneIndex = 3;
+    private static System.Random uniform = new System.Random();
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(uniform.Next(minSceneIndex, maxSceneIndex));
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Project3/Project3/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer mixer;
+   public void SetVolume(float volume)
+    {
+        mixer.SetFloat("masterVolume", volume);
+    }
+
+    public void SetQuality(int qualityLevel)
+    {
+        QualitySettings.SetQualityLevel(qualityLevel);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
