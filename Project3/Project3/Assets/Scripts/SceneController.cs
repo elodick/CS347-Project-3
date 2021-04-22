123071c013995265cb09a4e3ea0c22f1931592ab
jfletch1088 <46976616+jfletch1088@users.noreply.github.com>
2021-04-22 15:12:50 -0500

    Mainmenu connects to starterstage

--- Project3/Project3/Assets/Scripts/SceneController.cs
+++ Project3/Project3/Assets/Scripts/SceneController.cs
@@ -4,12 +4,20 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class SceneController : MonoBehaviour
 {
-    public string nextScene;
-    public 
+    private string[] scenes;
+    private int nextSceneIndex;
     // Start is called before the first frame update
     void Start()
     {
-        
+        scenes = new string[6];
+        scenes[0] = "Menu";
+        scenes[1] = "StarterStage";
+        scenes[2] = "Stage1_1";
+        scenes[3] = "Stage1_2";
+        scenes[4] = "Stage2_1";
+        scenes[5] = "BossStage";
+
+        nextSceneIndex = 1;
     }
 
     // Update is called once per frame
@@ -17,10 +25,11 @@ public class SceneController : MonoBehaviour
     {
         
     }
-
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        SceneManager.LoadScene(nextScene);
+        nextSceneIndex++;
+        SceneManager.LoadScene(scenes[nextSceneIndex]);
         GameObject.Find("Player").transform.position = new Vector3(0, 0, -1);
     }
 }
