d5aa67a17c4c106342e494000b7952e1322cf16c
thedillster <81203444+thedillster@users.noreply.github.com>
2021-04-22 21:24:59 -0500

    Boss stage finished except for Exit

---
+++ Project3/Project3/Assets/Scripts/Boss 1/ExitGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            //Exit game
+        }
+    }
+}
